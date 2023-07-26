bb8f9f81a46837bf9a3c5b61952c295802718409
tanughate05 <93458822+tanughate05@users.noreply.github.com>
2023-07-26 23:45:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
+	
     //Write your code here
+	var volume;
+	var radius = document.getElementById('radius').value;
+	radius = Math.abs(radius);
+	volume = (4/3 )* Math.PI * Math.pow(radius,3);
+	volume = volume.tofixed(4);
+	document.getElementById('volume').value = volume;
+	return false;
   
 } 
 
